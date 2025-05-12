4ec680d3d99b33d25f291fb7ccb9f16d501dccc4
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-05-13 00:07:32 +0700

    build: android-23 --> android-24
    #12

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -2,7 +2,7 @@ APP_STL := gnustl_static
 
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
-APP_PLATFORM := android-23
+APP_PLATFORM := android-24
 
 
 ifeq ($(NDK_DEBUG),1)
